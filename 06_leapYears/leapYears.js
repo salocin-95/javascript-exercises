23f2b689a5336ea0d0c94cdfac221ba0763004a0
salocin-95 <renicolas7@gmail.com>
2024-11-01 16:51:08 +0000

    Modify files

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if ((Number.isInteger(year/400)) === false) {
+        return false;
+    } else {
+        if ((year % 4) === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }  
 };
 
 // Do not edit below this line
