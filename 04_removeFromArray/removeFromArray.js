e3aa5a63879e0435d251dad54fb21dbde47edba0
salocin-95 <renicolas7@gmail.com>
2024-11-01 12:05:06 +0000

    Modify test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (array, ...input) => {
+    return array.filter((element) => element != input)
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
