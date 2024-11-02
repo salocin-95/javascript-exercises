23e20b661488a7b2f56e035820577ee61083731a
salocin-95 <renicolas7@gmail.com>
2024-11-02 14:59:21 +0000

    Modify files

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
-
+    for(i = 0, j = arguments.length - 1; i < arguments.length; i++, j--) {
+        if (arguments[i] != arguments[j]) {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
