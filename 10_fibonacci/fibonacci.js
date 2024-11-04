21cea220a74bfad3049cc5d7d655e43c2a3eb35f
salocin-95 <renicolas7@gmail.com>
2024-11-04 22:50:36 +0000

    Submit solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    number = parseInt(count)
+    if (number === 0) return 0;
+    if (number < 0) return 'OOPS';
+    let a = 0, b = 1;
+    for (i = 1; i < number; i++) {
+        const next = a + b;
+        a = b;
+        b = next
+    }
+    return b;
 
 };
 
