d0263c04591d22b780668153d9da0ba96c952e5e
salocin-95 <renicolas7@gmail.com>
2024-11-04 21:30:42 +0000

    Submit solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,19 @@
 const palindromes = function () {
-    for(i = 0, j = arguments.length - 1; i < arguments.length; i++, j--) {
-        if (arguments[i] != arguments[j]) {
+    let str = arguments[0].replace(/[^a-zA-Z0-9]/g, "").toLowerCase() // Regex removes all non-alphanumeric characters.
+    for(left = 0, right = str.length - 1; left < (str.length)/2 ; left++, right--) {
+        if (str[left] !== str[right]) {
             return false;
         }
     }
+    return true
+
+    /*    
+    for(i = 0, j = arguments.length - 1; i < arguments.length; i++, j--) {
+        if (arguments[0][i] !== arguments[0][j]) {
+            return false;
+        }
+        return true; // Este return iba afuera del loop
+    }*/
 };
 
 // Do not edit below this line
